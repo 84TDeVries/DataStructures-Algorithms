a6f8064040505a2622fd439fd52a76f845f557ca
84TDeVries <56742925+84TDeVries@users.noreply.github.com>
2019-10-18 15:56:15 -0600

    Delete MArs.py

--- MArs.py
+++
@@ -1,5 +0,0 @@
-year = 12*(int(input()) - 2018)
-if (year-3)%26 == 0 or (year-2)%26 == 0 or (year-1)%26 == 0 or (year)%26 == 0 or (year+1)%26 == 0 or (year+2)%26 == 0 or (year+3)%26 == 0 or (year+4)%26 == 0 or (year+5)%26 == 0 or (year+6)%26 == 0:
-    print("yes")
-else:
-    print("no")
